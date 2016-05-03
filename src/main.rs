7bd629c17d6a6432fc7d5b74a3c95f2b80e985c5
Rafał Michalski <royaltm75@gmail.com>
2016-05-03 23:40:03 +0200

    do not use threads for concurrency=1 and move parallel implementation to a function

--- src/main.rs
+++ src/main.rs
@@ -14,46 +14,51 @@ use library::{Int, Uint, Wall};
 fn main() {
     let mut args = env::args();
     args.next();
-    let cols     = if let Some(arg) = args.next() { arg.parse::<Int>() }   else { Ok(10) };
-    let rows     = if let Some(arg) = args.next() { arg.parse::<Int>() }   else { Ok(10) };
-    let deep     = if let Some(arg) = args.next() { arg.parse::<Uint>() }  else { Ok(1u32) };
-    let poolsize = if let Some(arg) = args.next() { arg.parse::<usize>() } else { Ok(num_cpus::get()) };
+    let cols = if let Some(arg) = args.next() { arg.parse::<Int>() }   else { Ok(10) };
+    let rows = if let Some(arg) = args.next() { arg.parse::<Int>() }   else { Ok(10) };
+    let deep = if let Some(arg) = args.next() { arg.parse::<Uint>() }  else { Ok(1u32) };
+    let concurrency = if let Some(arg) = args.next() { arg.parse::<usize>() } else { Ok(num_cpus::get()) };
 
-    match (rows, cols, deep, poolsize) {
-        (Ok(rows), Ok(cols), Ok(deep), Ok(poolsize)) => {
+    match (rows, cols, deep, concurrency) {
+        (Ok(rows), Ok(cols), Ok(deep), Ok(concurrency)) => {
             if rows > 0 && cols > 0 {
 
-                /* Linear implementation */
-                // for _ in 0..deep {
-                //     let mut wall = Wall::new(cols, rows);
-                //     wall.carve();
-                //     wall.print();
-                //     println!("");
-                // }
-
-                /* Parallel implementation */
-                let pool = ThreadPool::new(poolsize);
-                let port = {
-                    let (chan, port) = channel(); // sync_channel((deep as usize + poolsize - 1) / poolsize);
+                if concurrency <= 1 {
+                    /* Linear implementation */
                     for _ in 0..deep {
-                        let chan = chan.clone();
-                        pool.execute(move || {
-                            let mut wall = Wall::new(cols, rows);
-                            wall.carve();
-                            chan.send(wall).unwrap();
-                        });
+                        let mut wall = Wall::new(cols, rows);
+                        wall.carve();
+                        wall.print();
                     }
-                    port
-                };
-                for _ in 0..deep {
-                    let wall = port.recv().unwrap();
-                    wall.print();
+                } else {
+                    /* Parallel implementation */
+                    parallel(cols, rows, deep, concurrency);
                 }
 
             } else {
-                println!("Hey, give me numbers grater than 0");
+                println!("Hey, give me numbers bigger than 0");
             }
         }
         _ => println!("Hey, give me a number.")
+    }
+}
+
+fn parallel(cols: Int, rows: Int, deep: Uint, concurrency: usize) {
+    let pool = ThreadPool::new(concurrency);
+    let port = {
+        let (chan, port) = channel(); // sync_channel((deep as usize + concurrency - 1) / concurrency);
+        for _ in 0..deep {
+            let chan = chan.clone();
+            pool.execute(move || {
+                let mut wall = Wall::new(cols, rows);
+                wall.carve();
+                chan.send(wall).unwrap();
+            });
+        }
+        port
+    };
+    for _ in 0..deep {
+        let wall = port.recv().unwrap();
+        wall.print();
     }
 }
